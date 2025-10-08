f87674aaede912cdd5e6d87889f42cf2cb8254f5
Saima Khan <saimakhancsedu@gmail.com>
2025-10-08 08:24:06 +0600

    edited

---
+++ src/Pages/TrendingApss/TrendingApps.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TrendingApps = () => {
+  return <div></div>;
+};
+
+export default TrendingApps;
--- src/Pages/Trust/Trust.jsx
+++ src/Pages/Trust/Trust.jsx
@@ -2,22 +2,23 @@ import React from "react";
 
 const Trust = () => {
   return (
-    <div className="bg-[#9F62F2] text-white">
-      <div className="mx-auto p-10">
+    <div className="bg-[#9F62F2] text-white py-14 ">
+      <h1 className="text-3xl font-bold">Trusted by Millions, Built for You</h1>
+      <div className="mx-auto p-7 flex gap-14 justify-center ">
         <div className="sec1">
-          <p>Total Downloads</p>
-          <h1>29.6</h1>
-          <p>21% More Than Last Month</p>
+          <p className="mb-3">Total Downloads</p>
+          <h1 className="text-4xl font-bold">29.6M</h1>
+          <p className="mt-3">21% More Than Last Month</p>
         </div>
         <div className="sec1">
-          <p>Total Downloads</p>
-          <h1>29.6</h1>
-          <p>21% More Than Last Month</p>
+          <p className="mb-3">Total Reviews</p>
+          <h1 className="text-4xl font-bold">906K</h1>
+          <p className="mt-3">45% More Than Last Month</p>
         </div>
         <div className="sec1">
-          <p>Total Downloads</p>
-          <h1>29.6</h1>
-          <p>21% More Than Last Month</p>
+          <p className="mb-3">Total Apps</p>
+          <h1 className="text-4xl font-bold">132+</h1>
+          <p className="mt-3">31% More Than Last Month</p>
         </div>
       </div>
     </div>
