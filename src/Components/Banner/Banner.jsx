52b7b760befa98ad772b1122aaf2d38372d49c47
Saima Khan <saimakhancsedu@gmail.com>
2025-10-08 07:52:34 +0600

    edited

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import bannerImg from "../../assets/hero.png";
+import Trust from "../../Pages/Trust/Trust";
 
 const Banner = () => {
   return (
-    <div className="mx-auto text-center px-32 pt-10">
-      <img src={bannerImg} alt="" />
+    <div>
+      <div className="mx-auto text-center px-32 pt-10">
+        <img src={bannerImg} alt="" />
+      </div>
+      <Trust></Trust>
     </div>
   );
 };
---
+++ src/Pages/Trust/Trust.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Trust = () => {
+  return (
+    <div className="bg-[#9F62F2] text-white">
+      <div className="mx-auto p-10">
+        <div className="sec1">
+          <p>Total Downloads</p>
+          <h1>29.6</h1>
+          <p>21% More Than Last Month</p>
+        </div>
+        <div className="sec1">
+          <p>Total Downloads</p>
+          <h1>29.6</h1>
+          <p>21% More Than Last Month</p>
+        </div>
+        <div className="sec1">
+          <p>Total Downloads</p>
+          <h1>29.6</h1>
+          <p>21% More Than Last Month</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Trust;
