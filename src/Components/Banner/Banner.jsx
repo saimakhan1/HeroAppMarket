e1fe5d257e212cf41dcc1b00741faa5f182f9e80
Saima Khan <saimakhancsedu@gmail.com>
2025-10-10 07:09:45 +0600

    necessary format done

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,20 +1,3 @@
-// import React from "react";
-// import bannerImg from "../../assets/hero.png";
-// import Trust from "../../Pages/Trust/Trust";
-
-// const Banner = () => {
-//   return (
-//     <div>
-//       <div className="mx-auto text-center px-32 pt-10">
-//         <img src={bannerImg} alt="" />
-//       </div>
-//       <Trust></Trust>
-//     </div>
-//   );
-// };
-
-// export default Banner;
-
 import React from "react";
 import bannerImg from "../../assets/hero.png";
 import Trust from "../../Pages/Trust/Trust";
--- src/Pages/AppDetails/AppNotFound.jsx
+++ src/Pages/AppDetails/AppNotFound.jsx
@@ -1,18 +1,3 @@
-// import React from "react";
-
-// import appError from "../../assets/App-Error.png";
-
-// const AppNotFound = () => {
-//   return (
-//     <div className=" text-center mx-auto">
-//       <h1>App Not Found</h1>
-//       <img src={appError} alt="appNotFound" />
-//     </div>
-//   );
-// };
-
-// export default AppNotFound;
-
 import React from "react";
 import appError from "../../assets/App-Error.png";
 
